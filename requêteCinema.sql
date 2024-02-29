d05b3a98c01e865ff736922d22c5f810ae7e621c
sataev12 <sataevmagomed.eu@gmail.com>
2024-02-29 01:08:54 +0100

    Liste des films durée > 2h15

--- requêteCinema.sql
+++ requêteCinema.sql
@@ -1,5 +1,11 @@
 -- Information d'un film : titre, année, durée et réalisateur
-SELECT Film.Titre, Film.AnneSortFr, Film.Duree, Personne.Nom, Personne.Prenom
+SELECT Film.Titre, Film.AnneSortFr, SEC_TO_TIME(Film.Duree * 60) AS Duree_formatée, Personne.Nom, Personne.Prenom
 FROM Film
 INNER JOIN Realisateur ON Film.Id_Realisateur = Realisateur.Id_Realisateur
 INNER JOIN Personne ON Realisateur.id_personne = Personne.id_personne
+-- SELECT FORMAT(temps, 'HH:mm') AS heure_formatée FROM votre_table;
+-- Liste des films dont la durée excède 2h15 classés par durée (du + long au + court)
+SELECT Film.Titre, SEC_TO_TIME(Film.Duree * 60) AS Duree_formatée
+FROM Film 
+WHERE Film.Duree > 135;
+
